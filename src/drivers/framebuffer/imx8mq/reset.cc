866975a86264dc82d6eb68af2636422e7f60036a
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-06-17 16:11:42 +0200

    imx8mq_fb_drv: strictly split C/C++ in local lx_emul

--- src/drivers/framebuffer/imx8mq/reset.cc
+++ src/drivers/framebuffer/imx8mq/reset.cc
@@ -11,31 +11,12 @@
  * version 2.
  */
 
-#include <base/log.h>
+#include <lx_emul/reset.h>
 #include <lx_kit/env.h>
 
-struct device;
-struct reset_control;
-
-extern "C" struct reset_control *
-__devm_reset_control_get(struct device * dev, const char * id, int index,
-                         bool shared, bool optional, bool acquired);
-
-extern "C" int reset_control_deassert(struct reset_control * rstc);
-
-
-extern "C" struct reset_control * __devm_reset_control_get(struct device *,
-                                                           const char * id,
-                                                           int, bool, bool, bool)
+void lx_emul_reset_deassert(const char * name)
 {
-	return (struct reset_control*) id;
-}
-
-
-extern "C" int reset_control_deassert(struct reset_control * rstc)
-{
-	Genode::String<64> type("reset-pin,", (const char*)rstc);
+	Genode::String<64> type("reset-pin,", name);
 	Lx_kit::env().devices.for_each([&] (Lx_kit::Device & d) {
 		if (type == d.compatible()) { d.enable(); } });
-	return 0;
 }

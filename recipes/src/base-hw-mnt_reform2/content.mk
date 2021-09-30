991a3e33ed2cbc5193fe73f71c8a4ed6dfbc8e90
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-09-30 12:51:58 +0200

    imx8q_evk: add board support

--- recipes/src/base-hw-mnt_reform2/content.mk
+++ recipes/src/base-hw-mnt_reform2/content.mk
@@ -3,7 +3,7 @@ include $(GENODE_DIR)/repos/base-hw/recipes/src/base-hw_content.inc
 content: src/bootstrap/board/imx8q_evk/board.h src/core/board/imx8q_evk/board.h
 
 src/bootstrap/board/imx8q_evk/board.h:
-	$(mirror_from_base_hw_dir)
+	$(mirror_from_rep_dir)
 
 src/core/board/imx8q_evk/board.h:
-	$(mirror_from_base_hw_dir)
+	$(mirror_from_rep_dir)

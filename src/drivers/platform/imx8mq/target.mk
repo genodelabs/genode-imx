1f7a58eb178c156b884690bb7cf8bde2338b09a5
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-10-07 10:24:42 +0200

    Add platform_drv for i.MX 8MQ
    * Imported from formerly main Genode repository (repos/os)

---
+++ src/drivers/platform/imx8mq/target.mk
@@ -0,0 +1,17 @@
+TARGET   = imx8mq_platform_drv
+REQUIRES = arm_v8
+SRC_CC   = ccm.cc
+SRC_CC  += device.cc
+SRC_CC  += device_component.cc
+SRC_CC  += device_model_policy.cc
+SRC_CC  += imx_device.cc
+SRC_CC  += main.cc
+SRC_CC  += session_component.cc
+SRC_CC  += root.cc
+LIBS     = base
+
+PLATFORM_ARM_DIR := $(call select_from_repositories,src/drivers/platform/spec/arm)
+INC_DIR           = $(PRG_DIR) $(PLATFORM_ARM_DIR)
+
+vpath %.cc $(PRG_DIR)
+vpath %.cc $(PLATFORM_ARM_DIR)

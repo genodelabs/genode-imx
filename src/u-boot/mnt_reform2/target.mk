dfa317d5c179633a324f04113efe6dac5e7c879e
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-10-13 11:20:29 +0200

    mnt_reform2: update u-boot loader
    * Update source repository (URL has changed)
    * Do not patch u-boot to keep USB voltage (not necessary anymore)
    * Do not use complete 'bootm' command, DTB file is superfluent therefore

--- src/u-boot/mnt_reform2/target.mk
+++ src/u-boot/mnt_reform2/target.mk
@@ -1,7 +1,7 @@
-TARGET             = mnt_reform2_uboot
-REQUIRES           = arm_v8a
-CUSTOM_TARGET_DEPS = flash.bin
-UBOOT_CONTRIB_DIR  = $(call select_from_ports,mnt_reform2_uboot)/uboot
+TARGET              = mnt_reform2_uboot
+REQUIRES            = arm_v8a
+CUSTOM_TARGET_DEPS  = flash.bin
+UBOOT_CONTRIB_DIR  := $(call select_from_ports,mnt_reform2_uboot)/uboot
 
 unexport BOARD
 unexport MAKEFLAGS
@@ -9,4 +9,4 @@ unexport MAKEFLAGS
 $(CUSTOM_TARGET_DEPS):
 	cp $(UBOOT_CONTRIB_DIR)/bl31-iMX8MQ.bin $(PWD)/$(PRG_REL_DIR)/
 	cp $(UBOOT_CONTRIB_DIR)/mntreform-config $(PWD)/$(PRG_REL_DIR)/.config
-	$(MAKE) -C $(UBOOT_CONTRIB_DIR) O=$(PWD)/$(PRG_REL_DIR) ARCH=arm CROSS_COMPILE=$(CROSS_DEV_PREFIX) flash.bin
+	$(MAKE) -C $(UBOOT_CONTRIB_DIR) O=$(PWD)/$(PRG_REL_DIR) ARCH=arm CROSS_COMPILE=$(CROSS_DEV_PREFIX) KCFLAGS=-DMNTREFORM_BOOT_GENODE flash.bin

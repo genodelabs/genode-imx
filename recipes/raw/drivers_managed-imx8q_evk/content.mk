3ea43b9c1c27b22a57c0d76bf52fe1690b1995c2
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-10-18 12:46:13 +0200

    imx8q_evk: add recipes for driver* and sculpt pkgs

---
+++ recipes/raw/drivers_managed-imx8q_evk/content.mk
@@ -0,0 +1,11 @@
+content: drivers.config event_filter.config en_us.chargen \
+         special.chargen numlock_remap.config gpu_drv.config
+
+drivers.config numlock_remap.config event_filter.config:
+	cp $(REP_DIR)/recipes/raw/drivers_managed-imx8q_evk/$@ $@
+
+en_us.chargen special.chargen:
+	cp $(GENODE_DIR)/repos/os/src/server/event_filter/$@ $@
+
+gpu_drv.config:
+	echo "<config/>" > $@

8ed392cb955753155cfc4e41f101f2f3d42bc1cd
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-12-22 12:29:12 +0100

    imx8mq_platform_drv: adapt to origin code changes
    Ref genodelabs/genode#4359

--- src/drivers/platform/imx8mq/target.mk
+++ src/drivers/platform/imx8mq/target.mk
@@ -9,9 +9,9 @@ SRC_CC  += session_component.cc
 SRC_CC  += root.cc
 LIBS     = base
 
-PLATFORM_ARM_DIR := $(call select_from_repositories,src/drivers/platform/spec/arm)
-INC_DIR           = $(PRG_DIR) $(PLATFORM_ARM_DIR)
+PLATFORM_OS_DIR := $(dir $(call select_from_repositories,src/drivers/platform/device.cc))
+INC_DIR          = $(PRG_DIR) $(PLATFORM_OS_DIR)
 
 vpath main.cc $(PRG_DIR)
 vpath ccm.cc  $(PRG_DIR)
-vpath %.cc $(PLATFORM_ARM_DIR)
+vpath %.cc    $(PLATFORM_OS_DIR)

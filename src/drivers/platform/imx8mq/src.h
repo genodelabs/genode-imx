1cb89d0d39cffa7b528009f017cab90fa20c65c5
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-02-04 16:08:47 +0100

    imx8mq_platform_drv: fix reset switches
    When unifying the reset mechanism in between genode-allwinner and genode-imx
    a regression was caused in the system-reset-controller implementaion of the
    i.MX8MQ platform driver. Unfortunately, this was not observed, because
    currently it is used only by the display subsystem of the MNT Reform2 and
    not by nightly tests.

--- src/drivers/platform/imx8mq/src.h
+++ src/drivers/platform/imx8mq/src.h
@@ -35,58 +35,71 @@ struct Src : Genode::Attached_mmio
 
 	struct Pcie_1 : Register<0x2c, 32>
 	{
-		struct Pcie_phy_global  : Bitfield<1, 1> {};
-		struct Pcie_phy_button  : Bitfield<2, 1> {};
-		struct Pcie_apps_enable : Bitfield<6, 1> {};
+		struct Phy_global  : Bitfield<1, 1> {};
+		struct Phy_button  : Bitfield<2, 1> {};
+		struct Apps_enable : Bitfield<6, 1> {};
 	};
 
-	struct Pcie_2 : Register<0x48, 32> { };
+	struct Pcie_2 : Register<0x48, 32>
+	{
+		struct Phy_global  : Bitfield<1, 1> {};
+		struct Phy_button  : Bitfield<2, 1> {};
+		struct Apps_enable : Bitfield<6, 1> {};
+	};
 
-	template <typename REG>
+	template <typename REG, typename BITFIELD>
 	struct Reset_switch : Driver::Reset
 	{
 		Src                  & src;
-		typename REG::access_t mask;
 		typename REG::access_t on;
 		typename REG::access_t off;
 
 		Reset_switch(Src                  & src,
 		             Reset::Name const    & name,
-		             typename REG::access_t mask,
 		             typename REG::access_t on,
 		             typename REG::access_t off)
 		:
 			Driver::Reset(src.resets, name),
-			src(src), mask(mask), on(on), off(off) {}
+			src(src), on(on), off(off) {}
+
+		void _deassert() override {
+			src.write<BITFIELD>(on); }
+
+		void _assert() override {
+			src.write<BITFIELD>(off); }
+	};
+
+	template <typename REG>
+	struct Pcie_phy_switch : Driver::Reset
+	{
+		Src & src;
+
+		Pcie_phy_switch(Src               & src,
+		                Reset::Name const & name)
+		: Driver::Reset(src.resets, name), src(src) {}
 
 		void _deassert() override
 		{
-			typename REG::access_t v = src.read<REG>();
-			v &= mask;
-			v &= on;
-			src.write<REG>(v);
+			src.write<typename REG::Phy_button>(0);
+			src.write<typename REG::Phy_global>(0);
 		}
 
-		void _assert() override
-		{
-			typename REG::access_t v = src.read<REG>();
-			v &= mask;
-			v &= off;
-			src.write<REG>(v);
+		void _assert() override {
+			src.write<typename REG::Phy_button>(1);
+			src.write<typename REG::Phy_global>(1);
 		}
 	};
 
-	Reset_switch<Mipi_phy> mipi_dsi_byte { *this, "mipi_dsi_byte", Mipi_phy::Byte::mask(), 1, 0 };
-	Reset_switch<Mipi_phy> mipi_dsi_dpi  { *this, "mipi_dsi_dpi",  Mipi_phy::Dpi::mask(),  1, 0 };
-	Reset_switch<Mipi_phy> mipi_dsi_esc  { *this, "mipi_dsi_esc",  Mipi_phy::Esc::mask(),  1, 0 };
-	Reset_switch<Mipi_phy> mipi_dsi_pclk { *this, "mipi_dsi_pclk", Mipi_phy::Pclk::mask(), 1, 0 };
+	Reset_switch<Mipi_phy, Mipi_phy::Byte> mipi_dsi_byte { *this, "mipi_dsi_byte", 1, 0 };
+	Reset_switch<Mipi_phy, Mipi_phy::Dpi>  mipi_dsi_dpi  { *this, "mipi_dsi_dpi",  1, 0 };
+	Reset_switch<Mipi_phy, Mipi_phy::Esc>  mipi_dsi_esc  { *this, "mipi_dsi_esc",  1, 0 };
+	Reset_switch<Mipi_phy, Mipi_phy::Pclk> mipi_dsi_pclk { *this, "mipi_dsi_pclk", 1, 0 };
 
-	Reset_switch<Pcie_1> pcie_1_core { *this, "pcie_1_core", Pcie_1::Pcie_apps_enable::mask(), 1, 0 };
-	Reset_switch<Pcie_1> pcie_1_phy  { *this, "pcie_1_phy",  Pcie_1::Pcie_phy_button::mask() |
-	                                                         Pcie_1::Pcie_phy_global::mask(),  0, 6 };
-	Reset_switch<Pcie_2> pcie_2_core { *this, "pcie_2_core", Pcie_1::Pcie_apps_enable::mask(), 1, 0 };
-	Reset_switch<Pcie_2> pcie_2_phy  { *this, "pcie_2_phy",  Pcie_1::Pcie_phy_button::mask() |
-	                                                         Pcie_1::Pcie_phy_global::mask(),  0, 6 };
+	Reset_switch<Pcie_1, Pcie_1::Apps_enable> pcie_1_core { *this, "pcie_1_core", 1, 0 };
+	Pcie_phy_switch<Pcie_1>                   pcie_1_phy  { *this, "pcie_1_phy" };
+
+	Reset_switch<Pcie_2, Pcie_2::Apps_enable> pcie_2_core { *this, "pcie_2_core", 1, 0 };
+	Pcie_phy_switch<Pcie_2>                   pcie_2_phy  { *this, "pcie_2_phy" };
 
 	enum {
 		SRC_MMIO_BASE = 0x30390000,

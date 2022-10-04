e3c0180eb2af76e6d12fef113e1a706c7018793e
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-10-04 09:52:33 +0200

    imx53_platform_drv: use common target.inc makefile
    To cover changes of the generic platform driver this one is derived
    from use the common, generic makefile description.

--- src/drivers/platform/imx53/target.mk
+++ src/drivers/platform/imx53/target.mk
@@ -1,17 +1,4 @@
 TARGET   = imx53_platform_drv
 REQUIRES = arm_v7
-SRC_CC   = device.cc
-SRC_CC  += device_component.cc
-SRC_CC  += device_model_policy.cc
-SRC_CC  += device_pd.cc
-SRC_CC  += main.cc
-SRC_CC  += pci.cc
-SRC_CC  += session_component.cc
-SRC_CC  += root.cc
-LIBS     = base
 
-PLATFORM_OS_DIR := $(dir $(call select_from_repositories,src/drivers/platform/device.cc))
-INC_DIR          = $(PRG_DIR) $(PLATFORM_OS_DIR)
-
-vpath main.cc $(PRG_DIR)
-vpath %.cc    $(PLATFORM_OS_DIR)
+include $(call select_from_repositories,src/drivers/platform/target.inc)

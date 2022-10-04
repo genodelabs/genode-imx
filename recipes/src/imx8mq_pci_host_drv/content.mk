0981d4b504129abac7cd81c6af2949ec4a7a5506
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-10-04 09:51:25 +0200

    imx8mq_pci_host_drv: remove pci/*.h from src pkg
    It is now part of the os api package.
    
    Ref genodelabs/genode#4578

--- recipes/src/imx8mq_pci_host_drv/content.mk
+++ recipes/src/imx8mq_pci_host_drv/content.mk
@@ -1,8 +1,2 @@
 SRC_DIR = src/drivers/pci_host/imx8mq
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
-
-content: include/pci
-
-include/pci:
-	mkdir -p include
-	cp -r $(GENODE_DIR)/repos/os/$@ include/

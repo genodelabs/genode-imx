e59fb08f59c97f2cb27567fcc014dc5d62a6a559
Pirmin Duss <pirmin.duss@gapfruit.com>
2024-09-04 09:14:44 +0200

    drivers_nic-imx8mp_iot_gate: add depot archive
    Issue gapfruit#2174
    Issue genodelabs/genode-imx#18

---
+++ recipes/raw/drivers_nic-imx8mp_iot_gate/content.mk
@@ -0,0 +1,7 @@
+content: drivers.config system
+
+system:
+	cp $(REP_DIR)/recipes/raw/drivers_nic-imx8mp_iot_gate/$@ $@
+
+drivers.config:
+	cp $(REP_DIR)/recipes/raw/drivers_nic-imx8mp_iot_gate/$@ $@

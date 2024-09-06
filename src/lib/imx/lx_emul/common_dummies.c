724cf1381e78f7d29f85af87d5c18bd5955b2b96
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2024-09-06 19:42:40 +0200

    imx_lx_emul: remove shadow slub
    Remove shadow slub/slab from import-imx-lx_emul.mk. We use the original
    files now. In case you require the shadow implementation, add the files
    to your local project.
    
    issue genodelabs/genode#5264

--- src/lib/imx/lx_emul/common_dummies.c
+++ src/lib/imx/lx_emul/common_dummies.c
@@ -68,3 +68,38 @@ int arch_asym_cpu_priority(int cpu)
 	lx_emul_trace_and_stop(__func__);
 	return 0;
 }
+
+
+#include <linux/random.h>
+
+int __cold execute_with_initialized_rng(struct notifier_block * nb)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+#include <linux/sysctl.h>
+
+struct ctl_table_header * register_sysctl_sz(const char * path,struct ctl_table * table,size_t table_size)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+#include <linux/swiotlb.h>
+
+#ifdef CONFIG_SWIOTLB
+void swiotlb_dev_init(struct device * dev)
+{
+	lx_emul_trace(__func__);
+}
+
+
+bool is_swiotlb_allocated(void)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+#endif

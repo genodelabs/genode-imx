7710f71b005b367cc3238d7b90c6d69be540dd81
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2024-10-29 15:22:48 +0100

    driver/nic/stmmac: implement page_frag_alloc_align
    Got triggered when using Sculpt on the MNT Pocket, while
    downloading more packages.
    
    Issue genodelabs/genode#5360

--- src/driver/nic/stmmac/arm_v8a/generated_dummies.c
+++ src/driver/nic/stmmac/arm_v8a/generated_dummies.c
@@ -918,14 +918,6 @@ int __init of_setup_earlycon(const struct earlycon_id * match,unsigned long node
 }
 
 
-#include <linux/gfp.h>
-
-void * page_frag_alloc_align(struct page_frag_cache * nc,unsigned int fragsz,gfp_t gfp_mask,unsigned int align_mask)
-{
-	lx_emul_trace_and_stop(__func__);
-}
-
-
 #include <linux/reboot.h>
 
 enum reboot_mode panic_reboot_mode;
--- src/driver/nic/stmmac/arm_v8a/lx_emul.c
+++ src/driver/nic/stmmac/arm_v8a/lx_emul.c
@@ -110,9 +110,36 @@ EXPORT_SYMBOL(netdev_rss_key_fill);
 
 #include <linux/gfp.h>
 
+void * page_frag_alloc_align(struct page_frag_cache *nc,
+                             unsigned int fragsz, gfp_t gfp_mask,
+                             unsigned int align_mask)
+{
+	struct page *page;
+
+	if (fragsz > PAGE_SIZE) {
+		printk("no support for fragments larger than PAGE_SIZE\n");
+		lx_emul_backtrace();
+		return NULL;
+	}
+
+	page = __alloc_pages(gfp_mask, 0, 0, NULL);
+
+	if (!page)
+		return NULL;
+
+	return page->virtual;
+}
+
+
 void page_frag_free(void * addr)
 {
-	lx_emul_mem_free(addr);
+	struct page *page = virt_to_page(addr);
+	if (!page) {
+		printk("BUG %s: page for addr: %p not found\n", __func__, addr);
+		lx_emul_backtrace();
+	}
+
+	__free_pages(page, 0ul);
 }
 
 

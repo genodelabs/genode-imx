41b49acaf6b424a6e0b0d9fcf2a4c5c06fc7e549
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2025-02-20 17:26:10 +0100

    driver/nic/fec: enable imx8mp_armstone

--- src/driver/nic/fec/spec/arm_v8/target.mk
+++ src/driver/nic/fec/spec/arm_v8/target.mk
@@ -8,12 +8,14 @@ CC_OPT_gpio  += -DKBUILD_BASENAME='"gpio"'
 CC_OPT_gpio  += -DKBUILD_MODNAME='"gpio"'
 
 # Driver-specific device-tree binary data
-BOARDS                       := mnt_reform2 imx8q_evk imx8mp_iot_gate
+BOARDS                       := mnt_reform2 imx8q_evk imx8mp_armstone
 DTS_PATH(mnt_reform2)        := arch/arm64/boot/dts/freescale/imx8mq-mnt-reform2.dts
 DTS_EXTRACT(mnt_reform2)     := --select fec1
 DTS_PATH(imx8q_evk)          := arch/arm64/boot/dts/freescale/imx8mq-evk.dts
 DTS_EXTRACT(imx8q_evk)       := --select fec1
 DTS_PATH(imx8mp_iot_gate)    := arch/arm64/boot/dts/compulab/iot-gate-imx8plus.dts
 DTS_EXTRACT(imx8mp_iot_gate) := --select fec
+DTS_PATH(imx8mp_armstone)    := arch/arm64/boot/dts/F+S/armstonemx8mp.dts
+DTS_EXTRACT(imx8mp_armstone) := --select fec
 
 include $(REP_DIR)/src/driver/nic/fec/target.inc

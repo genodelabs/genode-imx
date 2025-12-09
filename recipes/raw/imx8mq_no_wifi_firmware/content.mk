522ceaeec16fdaafed9e617daca2435ed7418227
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2025-12-09 11:06:36 +0100

    sculpt: remove wifi firmware for MNT Reform2
    To keep the boot image small, and compatible to the pre-installed
    bootloader, we remove the wifi firmware from the common sculpt
    version for MNT Reform2, where most models use an Atheros card that
    doesn't need an additional firmware.

---
+++ recipes/raw/imx8mq_no_wifi_firmware/content.mk
@@ -0,0 +1,21 @@
+PORT_DIR := $(call port_dir,$(GENODE_DIR)/repos/dde_linux/ports/linux-firmware)
+
+content: ucode_files LICENSE.wifi imx8mq_wifi_firmware.tar
+
+
+.PHONY: ucode_files
+ucode_files:
+	cp $(PORT_DIR)/firmware/regulatory.db .
+	cp $(PORT_DIR)/firmware/regulatory.db.p7s .
+
+LICENSE.wifi:
+	for i in $(PORT_DIR)/firmware/LICEN*E.*; do \
+	echo "$${i##*/}:" >> $@; \
+	  cat $$i >> $@; \
+	  echo >> $@; \
+	done
+
+include $(GENODE_DIR)/repos/base/recipes/content.inc
+
+imx8mq_wifi_firmware.tar: ucode_files LICENSE.wifi
+	$(TAR) --remove-files -cf $@ -C . *.*
